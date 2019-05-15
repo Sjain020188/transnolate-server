b8ef0385eb18500c708512e1d86f08032a1f7244
Shruti Jain <shrutiitengg@gmail.com>
2019-05-15 09:12:13 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+  )
+);
+
+app.get("/user", (req, res) => {
+  res.send("Welcome to chatssapp");
+});
+
+var server = http.createServer(app);
+var io = socketio(server);
+
+io.on("connection", function(socket) {
+  var online = Object.keys(io.engine.clients);
+  console.log("online users", online);
+  io.emit("server message", JSON.stringify(online));
+
+  socket.on("disconnect", function() {
+    var online = Object.keys(io.engine.clients);
+    io.emit("server message", JSON.stringify(online));
+  });
+});
+server.listen(3000, () => console.log("we up."));
